a399e73af946c43b8ef9293996576c379dcc72bd
Stefan Droeschler <st.droeschler@ostfalia.de>
2014-05-23 10:05:41 -0400

    Bundle for cpan

---
+++ install/Bundle/LONCAPA.pm
@@ -0,0 +1,79 @@
+package Bundle::LONCAPA;
+
+$VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::LONCAPA
+
+=head1 SYNOPSIS
+
+This is used by make.
+
+=head1 CONTENTS
+
+IO::Socket:SSL
+
+LWP::Protocol::https
+
+Net::SSLeay
+
+Safe
+
+MongoDB
+
+Safe::Hole
+
+CGI::Cookie
+
+Math::Cephes
+
+Math::Random
+
+Cache::Memcached
+
+Locale::Maketext
+
+Lingua::Bork
+
+Lingua::PigLatin
+
+Data::Uniqid
+
+Time::y2038
+
+DateTime::Format::RFC3339
+
+Digest::MD5
+
+File::Util
+
+File::Touch
+
+JSON::DWIW
+
+Net::SSL
+
+DateTime
+
+DateTime::TimeZone
+
+DateTime::Format::RFC3339
+
+Hash::Merge
+
+Spreadsheet::XLSX
+
+Spreadsheet::ParseExcel
+
+Text::CSV_PP
+
+
+=head1 CONFIGURATION
+
+=head1 AUTHOR
+
